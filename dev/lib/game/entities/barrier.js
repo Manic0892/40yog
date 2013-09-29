c1ea6cb3c15c5af40506a0452ac4a86f4185edfc
Manic <basbalmanic@gmail.com>
2013-09-29 05:34:14 -0700

    Added paramaters to barrier

--- dev/lib/game/entities/barrier.js
+++ dev/lib/game/entities/barrier.js
@@ -3,6 +3,10 @@ ig.module('game.entities.barrier').requires('impact.entity').defines(function() 
 		size: {x:200,y:141},
 		animSheet: new ig.AnimationSheet('media/barrier.png', 200,141),
 		
+		type: ig.Entity.TYPE.NONE,
+		collides: ig.Entity.TYPE.STATIC,
+		checkAgainst: ig.Entity.TYPE.A,
+		
 		init: function(x,y,settings) {
 			this.parent(x,y,settings);
 			this.addAnim('idle', 1, [0]);
