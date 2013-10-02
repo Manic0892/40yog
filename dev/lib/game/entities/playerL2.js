e1cd728243c1705950fc9e16154a1eadf5790483
Manic <basbalmanic@gmail.com>
2013-10-02 03:43:17 -0700

    Added animSheet for car

--- dev/lib/game/entities/playerL2.js
+++ dev/lib/game/entities/playerL2.js
@@ -2,6 +2,8 @@ ig.module('game.entities.playerL2').requires('ig.entities.player').defines(funct
 	EntityPlayerL2 = EntityPlayer.extend({
 		type: ig.Entity.TYPE.A,
 		collides: ig.Entity.COLLIDES.ACTIVE,
-		checkAgainst: ig.Entity.TYPE.B
+		checkAgainst: ig.Entity.TYPE.B,
+		
+		animSheet: new ig.AnimationSheet( 'media/car.png', 294, 600 )
 	});
 });
