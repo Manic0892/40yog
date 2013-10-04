8b39b6c2f3b7361e9497ffec76cec2f9fa0f2135
Manic <basbalmanic@gmail.com>
2013-10-03 23:41:59 -0700

    Added playerL2 to level 2

--- dev/lib/game/entities/playerL2.js
+++ dev/lib/game/entities/playerL2.js
@@ -1,10 +1,12 @@
-ig.module('game.entities.playerL2').requires('ig.entities.player').defines(function() {
+ig.module('game.entities.playerL2').requires('game.entities.player').defines(function() {
 	EntityPlayerL2 = EntityPlayer.extend({
 		type: ig.Entity.TYPE.A,
 		collides: ig.Entity.COLLIDES.ACTIVE,
 		checkAgainst: ig.Entity.TYPE.B,
 		
 		animSheet: new ig.AnimationSheet( 'media/car.png', 294, 600 ),
+		
+		size: {x:294, y:600},
 		
 		init: function(x,y,settings) {
 			this.parent(x,y,settings);
