4a67a217ac93293bbb3cf0a6d35299e32c98015e
Manic <basbalmanic@gmail.com>
2013-10-04 22:46:16 -0700

    Car moves

--- dev/lib/game/entities/playerL2.js
+++ dev/lib/game/entities/playerL2.js
@@ -11,6 +11,11 @@ ig.module('game.entities.playerL2').requires('game.entities.player').defines(fun
 		init: function(x,y,settings) {
 			this.parent(x,y,settings);
 			this.addAnim('idle', 1, [0]);
+		},
+		
+		update: function() {
+			this.vel.x += 1;
+			this.parent();
 		}
 	});
 });
