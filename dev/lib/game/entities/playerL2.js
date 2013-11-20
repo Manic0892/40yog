e03daece7013168ff08d1caf1dd62b4b18715626
Manic <basbalmanic@gmail.com>
2013-11-20 00:13:18 -0800

    Can turn right and left.  Next: fix camera

--- dev/lib/game/entities/playerL2.js
+++ dev/lib/game/entities/playerL2.js
@@ -8,7 +8,7 @@ ig.module('game.entities.playerL2').requires('impact.entity').defines(function()
 		
 		maxVel: {x:750,y:200},
 		size: {x:261, y:128},
-		friction: {x:100,y:200},
+		friction: {x:0,y:400},
 		
 		health:100,
 		gravityFactor:0,
@@ -17,23 +17,32 @@ ig.module('game.entities.playerL2').requires('impact.entity').defines(function()
 			this.parent(x,y,settings);
 			this.addAnim('idle', 1, [0]);
 			this.currentAnim = this.anims.idle;
+			this.accel.x=100;
 		},
 		
 		update: function() {
-			this.accel = {x:0,y:0};
-			if (ig.input.state('left') && this.vel.y < 0)
-				this.accel.x = -400;
-			if (ig.input.state('right') && this.vel.y < 0)
-				this.accel.x = 400;
-			if (ig.input.state('up'))
-				this.accel.y = -200;
-			if (ig.input.state('down') && this.vel.y < 0)
-				this.accel.y = 300;
-			if (this.vel.y > 0)
-				this.vel.y = 0;
-			if (this.vel.y >= 0)
-				this.vel.x = 0;
+			//this.accel = {x:0,y:0};
+			//if (ig.input.state('left') && this.vel.y < 0)
+			//	this.accel.x = -400;
+			//if (ig.input.state('right') && this.vel.y < 0)
+			//	this.accel.x = 400;
+			//if (ig.input.state('up'))
+			//	this.accel.y = -200;
+			//if (ig.input.state('down') && this.vel.y < 0)
+			//	this.accel.y = 300;
+			//if (this.vel.y > 0)
+			//	this.vel.y = 0;
+			//if (this.vel.y >= 0)
+			//	this.vel.x = 0;
+			if (ig.input.state('left') || ig.input.state('up'))
+				this.vel.y = -200;
+			else if (ig.input.state('right') || ig.input.state('down'))
+				this.vel.y = 200;
 			this.parent();
+			//if (this.vel.x == this.maxVel.x)
+			//	this.accel.x = 0;
+			//else
+			//	this.accel.x = 100;
 		},
 		
 		draw: function() {
@@ -42,8 +51,8 @@ ig.module('game.entities.playerL2').requires('impact.entity').defines(function()
 		
 		check: function(other) {
 			this.receiveDamage(10);
-			this.vel.y += 500;
-			this.vel.x = 0;
+			//this.vel.y += 500;
+			this.vel.x -= 200;
 			//this.pos.y += 50;
 			other.kill();
 		},
