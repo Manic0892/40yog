3897f701969972b54891426da0d5d0113e9b853f
Manic <basbalmanic@gmail.com>
2013-12-03 06:00:17 -0800

    Got rid of redundant menu items in preparation for playtesting

--- dev/lib/game/entities/mainMenu.js
+++ dev/lib/game/entities/mainMenu.js
@@ -7,11 +7,11 @@ ig.module('game.entities.mainMenu').requires('game.entities.menu').defines(funct
 				ig.game.loadLevel(Level1);
 			}}, /*{text:'OPTIONS', exec:function() {
 				console.log('clicked options');
-			}},*/ {text:'HOW TO PLAY', exec:function() {
+			}}, {text:'HOW TO PLAY', exec:function() {
 				ig.game.loadLevel(LevelHowToPlay)
 			}}, {text: 'L2_DEBUG', exec:function() {
 				ig.game.loadLevel(Level2);
-			}}
+			}}*/
 		],
 		
 		initYOffset: 64,
