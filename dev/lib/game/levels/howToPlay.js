d2883ff34074d5f8cbdec4c930dec24ccbdf1c28
Manic <basbalmanic@gmail.com>
2013-12-17 02:31:30 -0800

    Removed redundant files.

--- dev/lib/game/levels/howToPlay.js
+++
@@ -1,5 +0,0 @@
-ig.module( 'game.levels.howToPlay' )
-.requires( 'impact.image','game.entities.howToPlay','game.entities.levelHowToPlayLogic' )
-.defines(function(){
-LevelHowToPlay=/*JSON[*/{"entities":[{"type":"EntityHowToPlay","x":172,"y":96},{"type":"EntityLevelHowToPlayLogic","x":340,"y":148}],"layer":[]}/*]JSON*/;
-});
