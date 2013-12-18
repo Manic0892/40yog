c4516c01b756194dcc881428b25b4747994ca9e6
Manic <basbalmanic@gmail.com>
2013-12-17 20:26:43 -0800

    Added logging info to build scripts.

--- build.sh
+++ build.sh
@@ -7,4 +7,9 @@ cd ./built
 mv game.min.js game.min.js.old
 echo 'Further minifying...'
 curl -X POST -s --data-urlencode 'input@game.min.js.old' http://javascript-minifier.com/raw > game.min.js
+INSIZE=$(du -b -h game.min.js.old)
+OUTSIZE=$(du -b -h game.min.js)
+INSIZE=${INSIZE%K*}"K"
+OUTSIZE=${OUTSIZE%K*}"K"
+echo $INSIZE" -> "$OUTSIZE
 rm game.min.js.old
--- build_node.sh
+++ build_node.sh
@@ -7,4 +7,9 @@ cd ./built
 mv game.min.js game.min.js.old
 echo 'Further minifying...  Requires UglifyJS2 (https://github.com/mishoo/UglifyJS2)'
 uglifyjs game.min.js.old --screw-ie8 --stats -o game.min.js
+INSIZE=$(du -b -h game.min.js.old)
+OUTSIZE=$(du -b -h game.min.js)
+INSIZE=${INSIZE%K*}"K"
+OUTSIZE=${OUTSIZE%K*}"K"
+echo $INSIZE" -> "$OUTSIZE
 rm game.min.js.old
