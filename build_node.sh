cdb3f2461f5236f48516e6698c517f121839be4a
Manic <basbalmanic@gmail.com>
2013-12-18 20:18:53 -0800

    Added jquery and jquery cookie libs

--- build_node.sh
+++ build_node.sh
@@ -3,6 +3,7 @@ cd ./dev/tools
 cd ../..
 mv ./dev/game.min.js ./built
 cp -r ./dev/media ./built/
+cp -r ./dev/lib_extern ./built
 cd ./built
 mv game.min.js game.min.js.old
 echo 'Further minifying...  Requires UglifyJS2 (https://github.com/mishoo/UglifyJS2)'
