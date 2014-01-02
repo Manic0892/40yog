85c314d0e687565d9a2e57dfa45741b7cb6d3ec0
Manic <basbalmanic@gmail.com>
2014-01-02 02:21:11 -0800

    Added sliders for sound and music volume control.  Fixes #21.

--- dev/lib/game/entities/pauseMenu.js
+++ dev/lib/game/entities/pauseMenu.js
@@ -5,42 +5,25 @@ ig.module('game.entities.pauseMenu').requires('game.entities.menu', 'game.entiti
 		
 		safetyCD: 20, //this is here to fix bug where it's spawned and then kills itself but misses toggling pause again.  dumb bug.
 		
-		statusFont: new ig.Font('media/bebas_neue_25_black.png'),
+		//statusFont: new ig.Font('media/bebas_neue_25_black.png'),
 		
 		font: new ig.Font('media/bebas_neue_50_black.png'),
 		redFont: new ig.Font('media/bebas_neue_50_red.png'),
+		zIndex:999,
 		
 		items: [
 			{text:'RESUME', exec:function() {
 				ig.game.togglePause();
 				ig.game.getEntitiesByType(EntityPauseMenu)[0].kill(); //Fuck, this is hacky.
 			}},
-			{text:'MUSIC +',exec:function() {
-				ig.music.volume += .1;
-				ig.music.volume = Math.ceil(ig.music.volume * 10) / 10;
-				if (ig.music.volume > 1) ig.music.volume = 1;
+			{text:'',exec:function() {
 			}},
-			{text:'MUSIC -',exec:function() {
-				ig.music.volume -= .1;
-				ig.music.volume = Math.floor(ig.music.volume * 10) / 10;
-				if (ig.music.volume < 0) ig.music.volume = 0;
+			{text:'',exec:function() {
 			}},
-			{text:'SOUND +',exec:function() {
-				ig.soundManager.volume += .1;
-				ig.soundManager.volume = Math.ceil(ig.soundManager.volume * 10) / 10;
-				if (ig.soundManager.volume > 1) ig.soundManager.volume = 1;
+			{text:'',exec:function() {
 			}},
-			{text:'SOUND -',exec:function() {
-				ig.soundManager.volume -= .1;
-				ig.soundManager.volume = Math.floor(ig.soundManager.volume * 10) / 10;
-				if (ig.soundManager.volume < 0) ig.soundManager.volume = 0;
+			{text:'',exec:function() {
 			}},
-			//{text:'TOGGLE MUSIC',exec:function() {
-			//	ig.game.toggleMusic();
-			//}},
-			//{text:'TOGGLE SOUND EFFECTS',exec:function() {
-			//	ig.game.toggleSound();
-			//}},
 			{text:'MAIN MENU', exec:function() {
 				ig.game.togglePause();
 				ig.game.loadLevelDeferred(LevelMainMenu);
@@ -58,7 +41,8 @@ ig.module('game.entities.pauseMenu').requires('game.entities.menu', 'game.entiti
 			this.parent(x,y,settings);
 			x = ig.game.screen.x + ig.system.width/2;
 			y = ig.game.screen.y + ig.system.height/2;
-			this.slider = ig.game.spawnEntity(EntitySlider, x, y);
+			this.musicSlider = ig.game.spawnEntity(EntityMusicSlider, x, y - 125);
+			this.soundSlider = ig.game.spawnEntity(EntitySoundSlider,x,y - 40);
 		},
 		
 		update: function() {
@@ -76,17 +60,46 @@ ig.module('game.entities.pauseMenu').requires('game.entities.menu', 'game.entiti
 				ig.system.context.rect(0, 0, ig.system.width, ig.system.height);
 				ig.system.context.fillStyle = '#5f5f5f';
 				ig.system.context.fill();
-				var soundStatus = "SOUND: " + (ig.soundManager.volume == 0 ? "MUTED" : ig.soundManager.volume*100 + "%");
-				var musicStatus = "MUSIC: " + (ig.music.volume == 0 ? "MUTED" : ig.music.volume*100 + "%");
-				this.statusFont.draw(soundStatus, 50, 50, ig.Font.ALIGN.LEFT);
-				this.statusFont.draw(musicStatus, ig.system.width-50, 50, ig.Font.ALIGN.RIGHT);
+				//var soundStatus = "SOUND: " + (ig.soundManager.volume == 0 ? "MUTED" : ig.soundManager.volume*100 + "%");
+				//var musicStatus = "MUSIC: " + (ig.music.volume == 0 ? "MUTED" : ig.music.volume*100 + "%");
+				//this.statusFont.draw(soundStatus, 50, 50, ig.Font.ALIGN.LEFT);
+				//this.statusFont.draw(musicStatus, ig.system.width-50, 50, ig.Font.ALIGN.RIGHT);
 			}
 			this.parent();
 		},
 		
 		kill: function() {
-			this.slider.kill();
+			this.musicSlider.kill();
+			this.soundSlider.kill();
 			this.parent();
+		}
+	});
+	
+	EntityMusicSlider = EntitySlider.extend({
+		title: 'Music',
+		
+		init: function(x,y,settings) {
+			console.log(this.initVal);
+			this.initVal = ig.music.volume;
+			console.log(this.initVal);
+			this.parent(x,y,settings);
+		},
+		
+		sliderLogic: function() {
+			ig.music.volume = this.handle.val;
+		}
+	});
+	
+	EntitySoundSlider = EntitySlider.extend({
+		title: 'Sound',
+		
+		init: function(x,y,settings) {
+			this.initVal = ig.soundManager.volume;
+			this.parent(x,y,settings);
+		},
+		
+		sliderLogic: function() {
+			ig.soundManager.volume = this.handle.val;
 		}
 	});
 });
--- dev/lib/game/entities/slider.js
+++ dev/lib/game/entities/slider.js
@@ -18,14 +18,13 @@ ig.module('game.entities.slider').requires('impact.entity').defines(function() {
 		_wmBoxColor: "rgba(255,255,255,0.3)",
 		size:{x:64,y:64},
 		
-		zIndex:1,
+		zIndex:10000,
 		gravityFactor:0,
 		ignorePause: true,
 		
 		init: function(x,y,settings) {
 			x -= this.width/2;
 			this.parent(x,y,settings);
-			this.absolute = settings.absolute;
 			if (!ig.global.wm) {
 				this.handle = ig.game.spawnEntity(EntitySliderHandle, this.pos.x, this.pos.y, {parent:this});
 				ig.game.sortEntitiesDeferred();
@@ -40,10 +39,8 @@ ig.module('game.entities.slider').requires('impact.entity').defines(function() {
 		draw: function() {
 			var x = this.pos.x;
 			var y = this.pos.y;
-			if (!this.absolute) {
-				x -= ig.game.screen.x;
-				y -= ig.game.screen.y;
-			}
+			x -= ig.game.screen.x;
+			y -= ig.game.screen.y;
 			ig.system.context.beginPath();
 			ig.system.context.rect(x,y,this.width, this.height);
 			var grd = ig.system.context.createLinearGradient(x,y,x+this.width,y+this.height);
@@ -78,14 +75,13 @@ ig.module('game.entities.slider').requires('impact.entity').defines(function() {
 	});
 	
 	EntitySliderHandle = ig.Entity.extend({
-		height:40,
+		height:45,
 		width:10,
 		strokeWidth: 1,
-		barColor: '#afafaf',
-		strokeColor: '#f2f2f2',
 		handleColor: '#afafaf',
+		strokeColor: '#000',
 		val:1,
-		zIndex:2,
+		zIndex:10001,
 		size:{x:16,y:16},
 		gravityFactor: 0,
 		ignorePause:true,
@@ -93,15 +89,16 @@ ig.module('game.entities.slider').requires('impact.entity').defines(function() {
 
 		
 		init: function(x,y,settings) {
-			//this.maxVel.x = this.speed;
-			//this.maxVel.y = this.speed;
 			this.parent(x,y,settings);
 			this.parent = settings.parent;
-			this.finalY = this.pos.y - this.parent.strokeWidth/2;
+			//this.finalY = this.pos.y + this.parent.height/2 - this.height/2;  Centers it on the slider.
+			this.finalY = this.pos.y;
 			this.minX = this.pos.x + this.strokeWidth;
 			this.maxX = this.pos.x + this.parent.width - this.strokeWidth - this.width;
 			this.size.x = this.width + this.strokeWidth;
 			this.size.y = this.height + this.strokeWidth;
+			
+			this.pos.x = (this.parent.initVal * (this.maxX - this.minX)) + this.minX;
 		},
 		
 		update: function() {
@@ -116,13 +113,11 @@ ig.module('game.entities.slider').requires('impact.entity').defines(function() {
 		draw: function() {
 			var x = this.pos.x;
 			var y = this.pos.y;
-			if (!this.absolute) {
-				x -= ig.game.screen.x;
-				y -= ig.game.screen.y;
-			}
+			x -= ig.game.screen.x;
+			y -= ig.game.screen.y;
 			ig.system.context.beginPath();
 			ig.system.context.rect(x,y,this.width, this.height);
-			ig.system.context.fillStyle = this.barColor;
+			ig.system.context.fillStyle = this.handleColor;
 			ig.system.context.fill();
 			ig.system.context.lineWidth = this.strokeWidth;
 			ig.system.context.strokeStyle= this.strokeColor;
