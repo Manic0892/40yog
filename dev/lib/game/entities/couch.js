4281571ec0d52c92a402a6ee24411e69903c123d
Manic <basbalmanic@gmail.com>
2014-01-16 04:44:55 -0800

    Commented couch.  #19

--- dev/lib/game/entities/couch.js
+++ dev/lib/game/entities/couch.js
@@ -1,3 +1,5 @@
+//Couch entity.  It's used to spawn bedbugs on level 1.
+
 ig.module('game.entities.couch').requires('impact.entity', 'game.entities.enemyBedbug').defines(function() {
 	EntityCouch = ig.Entity.extend({
 		animSheet: new ig.AnimationSheet('media/images/sprites/couch.png', 128, 64),
@@ -13,9 +15,9 @@ ig.module('game.entities.couch').requires('impact.entity', 'game.entities.enemyB
 		
 		zIndex: -10,
 		
-		bulletDamage: false,
+		bulletDamage: false, //Deprecated.  Level 1 used to be all about using bullets to kill bedbugs, but since then it has changed to be about using a flamethrower
 		
-		spawnCD: 3.5,
+		spawnCD: 3.5, //Time in seconds between there being a chance to spawn a bedbug
 				
 		init: function(x,y,settings) {
 			this.parent(x,y,settings);
@@ -31,6 +33,7 @@ ig.module('game.entities.couch').requires('impact.entity', 'game.entities.enemyB
 		update: function() {
 			this.parent();
 			
+			//Change animation based on how much it's been damaged
 			if (this.health >50) {
 				this.currentAnim = this.anims.init;
 			} else if (this.health > 0) {
@@ -39,6 +42,7 @@ ig.module('game.entities.couch').requires('impact.entity', 'game.entities.enemyB
 				this.currentAnim = this.anims.dead;
 			}
 			
+			//Every 3.5 seconds, this gives the couch a 50% chance to spawn a bedbug as long as the couch hasn't been burnt down to <0 health
 			if (this.spawnTimer.delta() >= 0 && this.health > 0) {
 				var playerPos = ig.game.getEntitiesByType(EntityPlayer)[0].pos;
 				if (Math.random() > .5 && playerPos.x > this.pos.x - 500 && playerPos.x < this.pos.x + 500 && playerPos.y > this.pos.y - 500 && playerPos.y < this.pos.y + 500) {
@@ -48,6 +52,7 @@ ig.module('game.entities.couch').requires('impact.entity', 'game.entities.enemyB
 			}
 		},
 		
+		//Spawn bedbug
 		spawnEnemy: function() {
 			ig.game.spawnEntity(EntityEnemyBedbug, this.pos.x, this.pos.y+32);
 		},
@@ -57,6 +62,7 @@ ig.module('game.entities.couch').requires('impact.entity', 'game.entities.enemyB
 			this.dead = true;
 		},
 		
+		//You used to be able to "capture" the sun and use it to kill couches and bedbugs, but was removed from the game due to not making much sesne.  This function has therefore been deprecated.
 		receiveSunDamage: function(damage, other) {
 			damage *= 4;
 			this.receiveDamage(damage, other);
