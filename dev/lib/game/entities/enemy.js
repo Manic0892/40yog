875310b0444c3be74836c2952ca53c26a74e0974
Manic <basbalmanic@gmail.com>
2014-01-20 23:46:57 -0800

    Commented enemy.  #19

--- dev/lib/game/entities/enemy.js
+++ dev/lib/game/entities/enemy.js
@@ -1,8 +1,10 @@
+//Enemy entity.  Every enemy inherits from this.  This basically just tells every enemy to be type B (enemy type), and to check against type A (player type).
+
 ig.module('game.entities.enemy').requires('game.entities.character').defines(function() {
 	EntityEnemy = EntityCharacter.extend({
 		type: ig.Entity.TYPE.B,
 		checkAgainst: ig.Entity.TYPE.A,
 		
-		bulletDamage: true
+		bulletDamage: true  //Probably deprecated, unless a player shooting bullets is re-integrated
 	});
 });
