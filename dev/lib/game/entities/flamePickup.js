682fbf1d77368ee8545c62e9ab368ef4d503c7cc
Manic <basbalmanic@gmail.com>
2014-01-24 04:40:43 -0800

    Commented flamePickup. #19

--- dev/lib/game/entities/flamePickup.js
+++ dev/lib/game/entities/flamePickup.js
@@ -1,3 +1,5 @@
+//Flamethrower pickup.  Inherits from weapon pickup, in case other weapns need to be picked up in other levels, like a gun.
+
 ig.module('game.entities.flamePickup').requires('game.entities.weaponPickup').defines(function() {
 	EntityFlamePickup = EntityWeaponPickup.extend({
 		size:{x:36, y:50},
@@ -5,6 +7,6 @@ ig.module('game.entities.flamePickup').requires('game.entities.weaponPickup').de
 		animSheet: new ig.AnimationSheet('media/images/sprites/blowtorch.png',36,50),
 		_wmIgnore: false,
 		
-		weaponType: 10210897109101 //flame
+		weaponType: 10210897109101 //Flame.  This is the base 10 ASCII encoding of "flame."  This might be changed to just a string later.
 	});
 });
