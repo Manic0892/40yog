3ca645c2250bc8f9a3c856b84b99095c5568b173
Manic <basbalmanic@gmail.com>
2014-01-31 03:30:19 -0800

    Commented level logic. #19

--- dev/lib/game/entities/levelLogic.js
+++ dev/lib/game/entities/levelLogic.js
@@ -1,3 +1,5 @@
+//Basic level logic entity.  Handles things like playing music and setting the cursor.
+
 ig.module('game.entities.levelLogic').requires('impact.entity', 'game.entities.pauseMenu', 'game.entities.cursor').defines(function() {
 	EntityLevelLogic = ig.Entity.extend({
 		size:{x:64,y:64},
@@ -5,13 +7,13 @@ ig.module('game.entities.levelLogic').requires('impact.entity', 'game.entities.p
 		_wmBoxColor: 'rgba(0, 0, 255, 0.4)',
 		
 		ignorePause: true,
-		safetyTimer: .25, //dumb bug
+		safetyTimer: .25,
 		
 		levelMusic: new ig.Sound('media/sounds/rock_loop.*', false),
 		
-		defaultCursor: 1,
+		defaultCursor: 1, //Crosshair
 		
-		zIndex: -999,
+		zIndex: -999, //Updated and drawn before everything else
 		
 		init: function(x,y,settings) {
 			this.parent(x,y,settings);
@@ -27,14 +29,14 @@ ig.module('game.entities.levelLogic').requires('impact.entity', 'game.entities.p
 		
 		update: function() {
 			this.parent();
-			if (ig.input.pressed('esc') && !ig.game.paused && this.safetyTimer.delta() >= 0) {
+			if (ig.input.pressed('esc') && !ig.game.paused && this.safetyTimer.delta() >= 0) { //Pause the game and display the pause menu
 				ig.game.togglePause();
 				ig.game.spawnEntity(EntityPauseMenu, 0, 0, {parentLevel: this});
 			}
 			this.updateScreenPos();
 		},
 		
-		updateScreenPos: function() {
+		updateScreenPos: function() { //Put the player in the center of the screen.  Should be updated with a better camera that pans when the player gets to level edges.
 			var player = ig.game.getEntitiesByType( EntityPlayer )[0];
 			if( player ) {
 				ig.game.screen.x = player.pos.x - ig.system.width/2;
@@ -42,18 +44,19 @@ ig.module('game.entities.levelLogic').requires('impact.entity', 'game.entities.p
 			}
 		},
 		
-		loadLevel: function() {
+		loadLevel: function() { //Stop the music when the next level is loaded
 			ig.music.stop();
 		}
 	});
 	
+	//Healthbar entity.  Draws the player's health in a red bar in the upper left corner.
 	EntityHealthbar = ig.Entity.extend({
 		_wmIgnore: true,
 		
 		draw: function() {
-			var playerEnt = (ig.game.getEntitiesByType(EntityPlayer)[0]);
+			var playerEnt = (ig.game.getEntitiesByType(EntityPlayer)[0]); //Get the player
 			
-			if (playerEnt) {
+			if (playerEnt) { //If it exists, calculate the health percentage
 				newhp = playerEnt.health;
 				maxhp = playerEnt.maxHealth;
 				if (newhp > maxhp)
@@ -64,6 +67,7 @@ ig.module('game.entities.levelLogic').requires('impact.entity', 'game.entities.p
 				var x = 10;
 				var y = 10;
 				
+				//Draw it in a bar in the top left corner
 				var rectWidth = newhp/maxhp * width;
 				ig.system.context.beginPath();
 				ig.system.context.rect(x, y, rectWidth, height);
