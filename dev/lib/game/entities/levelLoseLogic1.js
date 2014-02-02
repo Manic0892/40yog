476d084837a52363df254167a1c4aa59cd4bd79d
Manic <basbalmanic@gmail.com>
2014-02-02 03:26:02 -0800

    Commented level lose logic for levels 1 and 2. #19

--- dev/lib/game/entities/levelLoseLogic1.js
+++ dev/lib/game/entities/levelLoseLogic1.js
@@ -1,7 +1,9 @@
+//Level 1 lose logic.
+
 ig.module('game.entities.levelLoseLogic1').requires('game.entities.levelLoseLogic', 'game.levels.1').defines(function(){
 	EntityLevelLoseLogic1 = EntityLevelLoseLogic.extend({
-		text: "You succumbed.\nPlay again?",
+		text: "You succumbed.\nPlay again?", //Succumbed to bedbugs
 		
-		thisLevel:Level1
+		thisLevel:Level1 //Reload level 1
 	});
 });
--- dev/lib/game/entities/levelLoseLogic2.js
+++ dev/lib/game/entities/levelLoseLogic2.js
@@ -1,7 +1,9 @@
+//Level 2 lose logic.
+
 ig.module('game.entities.levelLoseLogic2').requires('game.entities.levelLoseLogic', 'game.levels.2').defines(function(){
 	EntityLevelLoseLogic2 = EntityLevelLoseLogic.extend({
-		text: "You crashed.\nPlay again?",
+		text: "You crashed.\nPlay again?", //Crashed the car
 		
-		thisLevel:Level2
+		thisLevel:Level2 //Reload level 2
 	});
 });
