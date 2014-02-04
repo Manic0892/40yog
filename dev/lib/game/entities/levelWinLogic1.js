2170e58ab99c7333c65fed55f6aad21f39aab200
Manic <basbalmanic@gmail.com>
2014-02-04 02:12:51 -0800

    Commented level win logic for levels 1 and 2. #19

--- dev/lib/game/entities/levelWinLogic1.js
+++ dev/lib/game/entities/levelWinLogic1.js
@@ -1,10 +1,12 @@
+//Win logic for level 1
+
 ig.module('game.entities.levelWinLogic1').requires('game.entities.levelWinLogic', 'game.levels.intro2').defines(function(){
 	EntityLevelWinLogic1 = EntityLevelWinLogic.extend({
-		text: "Lil' Schmitty defeated the bedbugs.\nNow it's time to make some money.",
+		text: "Lil' Schmitty defeated the bedbugs.\nNow it's time to make some money.", //Sets up the next level, where you're a barbecue delivery guy
 		
-		nextLevel:LevelIntro2,
+		nextLevel:LevelIntro2, //The next level to load.  It's the intro for the next full level.
 		
-		beat: "l1"
+		beat: "l1" //Cookie for level select menu
 	});
 	
 	
--- dev/lib/game/entities/levelWinLogic2.js
+++ dev/lib/game/entities/levelWinLogic2.js
@@ -1,10 +1,12 @@
+//Win logic for level 2
+
 ig.module('game.entities.levelWinLogic2').requires('game.entities.levelWinLogic').defines(function(){
 	EntityLevelWinLogic2 = EntityLevelWinLogic.extend({
-		text: "Congratulations!\nYou beat the game.\n",
+		text: "Congratulations!\nYou beat the game.\n", //Level 2 is the last level currently
 		
-		nextLevel:null,
+		nextLevel:null, //Level 2 is the last level currently
 		
-		beat: "l2"
+		beat: "l2" //Cookie for level select menu
 	});
 	
 	
