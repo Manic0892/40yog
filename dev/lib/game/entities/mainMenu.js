06888b443084d7a6e230920cab4fbe6bf748a668
Manic <basbalmanic@gmail.com>
2014-02-06 02:53:18 -0800

    Commented main menu. #19

--- dev/lib/game/entities/mainMenu.js
+++ dev/lib/game/entities/mainMenu.js
@@ -1,14 +1,16 @@
+//Main Menu.  Appears as the first interactive part of the game, with entries to play from level 1, select a level, or change options.
+
 ig.module('game.entities.mainMenu').requires('game.entities.menu').defines(function(){
 	EntityMainMenu = EntityMenu.extend({
 		name: 'mainMenu',
 		
 		items: [
 			{text:'PLAY',exec:function() {
-				ig.game.loadLevelDeferred(LevelIntro1);
+				ig.game.loadLevelDeferred(LevelIntro1); //Load the intro for the first level
 			}}, {text:'LEVEL SELECT', exec:function() {
-				ig.game.loadLevelDeferred(LevelSelectMenu);
+				ig.game.loadLevelDeferred(LevelSelectMenu); //Load the level select menu
 			}}, {text:'OPTIONS', exec:function() {
-				ig.game.loadLevelDeferred(LevelOptionsMenu);
+				ig.game.loadLevelDeferred(LevelOptionsMenu); //Load the options menu
 			}}
 		],
 		
