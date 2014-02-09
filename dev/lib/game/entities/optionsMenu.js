9f786cb8602736e12938af1c801ba3f1274b4fe0
Manic <basbalmanic@gmail.com>
2014-02-09 03:59:36 -0800

    Finished commenting optionsMenu. #19

--- dev/lib/game/entities/optionsMenu.js
+++ dev/lib/game/entities/optionsMenu.js
@@ -7,6 +7,7 @@ ig.module('game.entities.optionsMenu').requires('game.entities.menu', 'game.enti
 		font: new ig.Font('media/fonts/bebas_neue_100_black.png'),
 		redFont: new ig.Font('media/fonts/bebas_neue_100_red.png'),
 		
+		//Start with an option to return to the main menu
 		items: [
 			{text:'MAIN MENU', exec:function() {
 				ig.game.loadLevelDeferred(LevelMainMenu);
@@ -24,14 +25,17 @@ ig.module('game.entities.optionsMenu').requires('game.entities.menu', 'game.enti
 			if (!ig.global.wm) {
 				x = ig.game.screen.x + ig.system.width/2;
 				y = ig.game.screen.y + ig.system.height/2;
+				//Add music and sound sliders
 				this.musicSlider = ig.game.spawnEntity(EntityMusicSliderOptions, x + this.initXOffset, 50);
 				this.soundSlider = ig.game.spawnEntity(EntitySoundSliderOptions, x + this.initXOffset, 150);
 			}
 		},
 		
+		//Spawn the rest of the menu items.  This is more specialized than the default spawnMenuItems because we need to add extra options
 		spawnMenuItems: function(settings) {
 			var text;
-			ig.global.graphics.gradient == true ? text = "GRADIENT ON" : text = "GRADIENT OFF";
+			ig.global.graphics.gradient == true ? text = "GRADIENT ON" : text = "GRADIENT OFF"; //Change the text for the gradient setting based on the current gradient setting
+			//Create a function which will both set the option and the cookie for the gradient
 			var exec = function() {
 				ig.global.graphics.gradient = !ig.global.graphics.gradient;
 				ig.global.graphics.gradient == true ? this.text = "GRADIENT ON" : this.text = "GRADIENT OFF";
@@ -41,7 +45,7 @@ ig.module('game.entities.optionsMenu').requires('game.entities.menu', 'game.enti
 			var height = this.font.heightForString("GRADIENT OFF");
 			var xPos = this.initXOffset + ig.system.width/2 - width/2 + ig.game.screen.x;
 			var yPos = this.initYOffset+-1*this.ySpacing + ig.game.screen.y;
-			this.menuItems.push(ig.game.spawnEntity(EntityMenuItemGraphicsOptions, xPos, yPos, {width:width, height:height, text: text, exec: exec, font: this.font, redFont: this.redFont, initXOffset: this.initXOffset}));
+			this.menuItems.push(ig.game.spawnEntity(EntityMenuItemGraphicsOptions, xPos, yPos, {width:width, height:height, text: text, exec: exec, font: this.font, redFont: this.redFont, initXOffset: this.initXOffset})); //Use a special menu item entity that will change its with on updating to correctly draw GRAIDNET ON and GRADIENT OFF to the proper width and centered
 			for (var i = 0; i < this.items.length; i++) {
 				width = this.font.widthForString(this.items[i].text);
 				height = this.font.heightForString(this.items[i].text);
@@ -61,6 +65,7 @@ ig.module('game.entities.optionsMenu').requires('game.entities.menu', 'game.enti
 			if (ig.input.pressed("lbtn") && this.selected)
 				this.exec();
 			this.selected = false;
+			//Fix the size depending on how wide the text CURRENTLY is, and fix the position to make sure it's still centered
 			var width = this.font.widthForString(this.text);
 			this.size.x = width;
 			this.pos.x = this.initXOffset + ig.system.width/2 - width/2 + ig.game.screen.x;
@@ -82,10 +87,11 @@ ig.module('game.entities.optionsMenu').requires('game.entities.menu', 'game.enti
 		titleFont: new ig.Font('media/fonts/bebas_neue_40_black.png'),
 		
 		init: function(x,y,settings) {
-			this.initVal = ig.music.volume;
+			this.initVal = ig.music.volume; //Set the slider to the correct position for the current music volume
 			this.parent(x,y,settings);
 		},
 		
+		//Set the music volume and a cookie to persist across play sessions
 		sliderLogic: function() {
 			ig.music.volume = this.handle.val;
 			$.cookie("music", ig.music.volume, {expires: 99999, path:'/'});
@@ -99,10 +105,11 @@ ig.module('game.entities.optionsMenu').requires('game.entities.menu', 'game.enti
 		titleFont: new ig.Font('media/fonts/bebas_neue_40_black.png'),
 		
 		init: function(x,y,settings) {
-			this.initVal = ig.soundManager.volume;
+			this.initVal = ig.soundManager.volume; //Set the slider to the correct position for the current sound volume
 			this.parent(x,y,settings);
 		},
 		
+		//Set the sound volume and a cookie to persist across play sessions
 		sliderLogic: function() {
 			ig.soundManager.volume = this.handle.val;
 			$.cookie("sound", ig.soundManager.volume, {expires: 99999, path:'/'});
