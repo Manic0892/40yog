7d76330bdc17ad39c533f2d4c673c9cd0c108d51
Sean McGeer <basbalmanic@gmail.com>
2014-02-26 23:19:34 -0800

    Continued commenting playerL1. #19

--- dev/lib/game/entities/playerL1.js
+++ dev/lib/game/entities/playerL1.js
@@ -49,16 +49,17 @@ ig.module('game.entities.playerL1').requires('game.entities.player', 'game.entit
 		
 		shoot: function() {
 			if (this.flameActive) {
-				ig.game.spawnEntity( EntityFireParticleDamage, this.pos.x+this.size.x/2, this.pos.y+this.size.y/2, {flip:this.flip, d:{x:ig.input.mouse.x, y:ig.input.mouse.y}, vel:this.vel} );
-				if (this.soundTimer.delta() >= 0) {
+				ig.game.spawnEntity( EntityFireParticleDamage, this.pos.x+this.size.x/2, this.pos.y+this.size.y/2, {flip:this.flip, d:{x:ig.input.mouse.x, y:ig.input.mouse.y}, vel:this.vel} ); //Spawn the fire particle at the end of the gun and give it the x and y of the mouse to help it draw a line for its path
+				if (this.soundTimer.delta() >= 0) { //If we are done with the timer between fire sound effects, play it again.  This prevents it from looping every time a fire particle is launched
 					this.fireSound.play();
 					this.soundTimer.reset();
 				}
 			}
 		},
 		
+		//Pick up powerups and weapons
 		pickup: function(other) {
-			if (other == 10210897109101) {
+			if (other == 10210897109101) { //Pick up flamethrower
 				this.arm.pickupFlame();
 				this.flameActive = true;
 			}
@@ -66,14 +67,15 @@ ig.module('game.entities.playerL1').requires('game.entities.player', 'game.entit
 		
 		receiveDamage: function(amount, from) {
 			this.parent(amount, from);
-			this.gruntSound.play();
+			this.gruntSound.play(); //Play the grunt sound effect on taking damage.  This should potentially only happen when the player isn't invincible.
 		},
 		
 		endOfLevel: function(win) {
-			win ? ig.game.loadLevelDeferred(LevelWin1) : ig.game.loadLevelDeferred(LevelLose1);
+			win ? ig.game.loadLevelDeferred(LevelWin1) : ig.game.loadLevelDeferred(LevelLose1); //If the player won, load the win level--otherwise, load the lose level
 		}
 	});
 	
+	//This might be deprecated in future versions, as referenced in issue #44. https://github.com/Manic0892/40yog/issues/44
 	EntityArmL1 = EntityArm.extend({
 		animSheet: new ig.AnimationSheet('media/images/sprites/arm_l1.png',24,8),
 		size: {x:24,y:8},
@@ -81,8 +83,8 @@ ig.module('game.entities.playerL1').requires('game.entities.player', 'game.entit
 		init: function(x,y,settings) {
 			this.parent(x,y,settings);
 			
-			this.addAnim('empty', 1, [0]);
-			this.addAnim('blowtorch', 1, [1]);
+			this.addAnim('empty', 1, [0]); //Without flamethrower
+			this.addAnim('blowtorch', 1, [1]); //With flamethrower
 			
 			this.currentAnim = this.anims.empty;
 		},
@@ -90,8 +92,9 @@ ig.module('game.entities.playerL1').requires('game.entities.player', 'game.entit
 		update: function() {
 			this.currentAnim.flip.x = this.flip;	
 			
-			var angle = Math.atan2(ig.input.mouse.y - this.pos.y + ig.game.screen.y, ig.input.mouse.x - this.pos.x + ig.game.screen.x);
+			var angle = Math.atan2(ig.input.mouse.y - this.pos.y + ig.game.screen.y, ig.input.mouse.x - this.pos.x + ig.game.screen.x); //Get the angle between the player and the mouse
 			
+			//Correct position and rotation based on the current position and flip
 			if (this.flip) {
 				this.pos.x -= this.attachedTo.size.x - 10;
 				this.currentAnim.pivot.x = this.size.x;
@@ -106,6 +109,7 @@ ig.module('game.entities.playerL1').requires('game.entities.player', 'game.entit
 			this.parent();
 		},
 		
+		//Pick up the flamethrower
 		pickupFlame: function() {
 			this.currentAnim = this.anims.blowtorch;
 		}
