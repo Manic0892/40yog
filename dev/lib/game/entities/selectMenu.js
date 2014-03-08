4f3c263934d7f4c3a1badf632f4670350e9ccbba
Sean McGeer <basbalmanic@gmail.com>
2014-03-08 02:12:47 -0800

    Commented level select menu. #19

--- dev/lib/game/entities/selectMenu.js
+++ dev/lib/game/entities/selectMenu.js
@@ -1,7 +1,10 @@
+//Level select menu.  Lists all levels the player's unlocked.
+
 ig.module('game.entities.selectMenu').requires('game.entities.menu').defines(function(){
 	EntitySelectMenu = EntityMenu.extend({
 		name: 'levelSelectMenu',
 		
+		//Level one should always be added by default
 		items: [
 			{text:'Level 1',exec:function() {
 				ig.game.loadLevelDeferred(LevelIntro1);
@@ -13,13 +16,14 @@ ig.module('game.entities.selectMenu').requires('game.entities.menu').defines(fun
 		initXOffset: 256,
 		
 		init: function(x,y,settings) {
+			//Check if the person has beat the preceding levels and then allow them to load the next one if they have
 			if ($.cookie("l1") == "beat") {
 				this.items.push({text:'Level 2', exec: function() {
 					ig.game.loadLevelDeferred(LevelIntro2);
 				}});
 			}
 			
-			
+			//Add link to go back to the main menu
 			this.items.push({text:'Main Menu', exec: function() {
 				ig.game.loadLevelDeferred(LevelMainMenu);
 			}});
