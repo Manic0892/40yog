cc4252c6c4c312eae6cc3c81f747a0f82009621f
Sean McGeer <basbalmanic@gmail.com>
2014-03-14 04:28:01 -0700

    Finished commenting code. #19

--- dev/lib/game/entities/winFlag.js
+++ dev/lib/game/entities/winFlag.js
@@ -1,7 +1,9 @@
+//Win flag used on first level.  Represents the final part of the level that the player is trying to get to.  Inherits from staticImage.
+
 ig.module('game.entities.winFlag').requires('game.entities.staticImage').defines(function() {
 	EntityWinFlag = EntityStaticImage.extend({
 		_wmIgnore: false,
 		
-		animSheet: new ig.AnimationSheet('media/images/sprites/winFlag.png',64,64)
+		animSheet: new ig.AnimationSheet('media/images/sprites/winFlag.png',64,64) //Little flag that says "Win!" on it.  This should be changed.
 	});
 });
