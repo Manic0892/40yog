64bfcae029c670e5fe7bf4c9c82c2e09156ad021
Sean McGeer <basbalmanic@gmail.com>
2014-03-13 22:12:42 -0700

    Commented weaponPickup. #19

--- dev/lib/game/entities/weaponPickup.js
+++ dev/lib/game/entities/weaponPickup.js
@@ -1,3 +1,5 @@
+//Weapon pickup entity.  Inherits from powerup.
+
 ig.module('game.entities.weaponPickup').requires('game.entities.powerup').defines(function() {
 	EntityWeaponPickup = EntityPowerup.extend({
 		size:{x:64, y:64},
@@ -8,10 +10,10 @@ ig.module('game.entities.weaponPickup').requires('game.entities.powerup').define
 		
 		animSheet: new ig.AnimationSheet('media/images/null.png',64,64),
 		
-		bobTime: 1.5,
+		bobTime: 1.5, //Time it takes to bob up and down
 		
 		powerup: function(other) {
-			if (other.pickup) {
+			if (other.pickup) { //If the other entity is a player, trigger the pickup for this and despawn the entity
 				other.pickup(this.weaponType);
 				this.kill();
 			}
