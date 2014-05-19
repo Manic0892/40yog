89a6fa163272a0b4bbaf2a7fccd1a58dbad04724
Manic <basbalmanic@gmail.com>
2014-05-19 02:41:08 -0700

    Rebuilt.

--- build_simple.sh
+++ build_simple.sh
@@ -3,7 +3,7 @@ cd ./dev/tools
 cd ../..
 mv ./dev/game.min.js ./built/lib/game.min.js
 cp -r ./dev/media ./built/
-cp -r ./dev/lib/utilities ./built/lib
+#cp -r ./dev/lib/utils ./built/lib
 cd ./built
 rm media/.gitignore
 rm -r media/.git
