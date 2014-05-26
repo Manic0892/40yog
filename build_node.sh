e51f0f38c129aa9979d6ae0ca3828738f6e0ed4c
Manic <basbalmanic@gmail.com>
2014-05-26 14:00:00 +0100

    Modified output messages on build scripts.

--- build_node.sh
+++ build_node.sh
@@ -1,6 +1,6 @@
 ./build_simple.sh
 cd ./built/lib
 mv game.min.js game.min.js.old
-echo 'Further minifying...  Requires UglifyJS2 (https://github.com/mishoo/UglifyJS2)'
+echo 'Minifying further...  Requires UglifyJS2 (https://github.com/mishoo/UglifyJS2).'
 uglifyjs game.min.js.old --screw-ie8 --stats -o game.min.js
 rm game.min.js.old
