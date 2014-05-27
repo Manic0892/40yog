f167fecf506a9db5465532cecf45d91fe7401b44
Manic <basbalmanic@gmail.com>
2014-05-27 02:24:56 -0700

    Typo.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./build_simple.sh
 cd ./built/lib
 mv game.min.js game.min.js.old
-echo 'Minifying further... Rquires that http://javascript-minifier.com is up.'
+echo 'Minifying further... Requires that http://javascript-minifier.com is up.'
 curl -X POST -s --data-urlencode 'input@game.min.js.old' http://javascript-minifier.com/raw > game.min.js
 rm game.min.js.old
